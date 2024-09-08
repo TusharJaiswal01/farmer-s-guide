79aaf57beae5e199927799ae0765a459259f8ef5
tushar jaiswal <tusharjaiswaltj01@gmail.com>
2024-09-08 23:45:22 +0530

    add disclaimer in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
-
 import React from 'react';
 
 function Footer() {
     return (
-        <footer className="bg-green-700 p-4 text-white mt-6">
+        <footer className="bg-green-700 p-8 text-white mt-6">
             <div className="container mx-auto text-center">
-                <p>&copy; 2024 Farmer's Guide. All rights reserved.</p>
+                {/* Warning Section */}
+                <div className="bg-yellow-400 text-red-700 p-4 mb-4 rounded-md">
+                    <p className="font-bold">
+                        Disclaimer: The information and data on this website are for demonstration purposes only.
+                        Please do not use this data for real farming decisions.
+                    </p>
+                </div>
+
+                {/* Copyright Section */}
+                <p className="text-sm md:text-lg">&copy; 2024 Farmer's Guide. All rights reserved.</p>
             </div>
         </footer>
     );
